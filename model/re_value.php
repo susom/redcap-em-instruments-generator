b1c305ee29ae87e49361cb3d2d522b7811f24e4b
Ihab Zeedia <ihab.zeedia@gmail.cm>
2020-05-04 16:40:27 -0700

    remove functions.php because it not needed anymore.

--- model/re_value.php
+++ model/re_value.php
@@ -6,7 +6,6 @@ namespace Stanford\InstrumentsGenerator;
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-require __DIR__ . "/../../../redcap_v9.2.5/Design/functions.php";
 try {
     if (!$_POST) {
         throw new \LogicException("You cant be here");
