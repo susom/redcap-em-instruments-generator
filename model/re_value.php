00711ccfb996cb127209d35a10ff28040eca75d4
Ihab Zeedia <ihab.zeedia@gmail.cm>
2020-08-31 10:26:26 -0700

    add comment.

--- model/re_value.php
+++ model/re_value.php
@@ -3,6 +3,7 @@
 namespace Stanford\InstrumentsGenerator;
 
 /** @var \Stanford\InstrumentsGenerator\InstrumentsGenerator $module */
+// hide comment.
 //ini_set('display_errors', 1);
 //ini_set('display_startup_errors', 1);
 //error_reporting(E_ALL);
